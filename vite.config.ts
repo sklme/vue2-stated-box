6abd8e412fb07b1240eef4512321defdda4c3f31
wrynnsun <wrynnsun@tencent.com>
2022-08-03 15:15:06 +0800

    feat: typings

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
 import { resolve } from 'node:path';
 import WindiCSS from 'vite-plugin-windicss';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue(), WindiCSS(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
       name: 'DisplayBox',
-      // the proper extensions will be added
       fileName: 'display-box',
     },
     rollupOptions: {
