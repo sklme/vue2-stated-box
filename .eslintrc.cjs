ce9b83f41ccfe75d1c3ff4b10bd095872db5311a
wrynnsun <wrynnsun@tencent.com>
2022-08-03 17:07:34 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   overrides: [
     {
       // 根目录层级的*.ts没有纳入tsconfig的project，所以在这里设置为project为null，不进行type checking
-      files: ['./*.ts', './scripts/*.ts', './dist/*'],
+      files: ['./*.ts', './scripts/*.ts', './dist/**/*'],
       env: {
         node: true,
       },
