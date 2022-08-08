581715a3fd5f73cf3e0011d489b18dff4d23bc0b
wrynnsun <wrynnsun@tencent.com>
2022-08-08 11:46:50 +0800

    feat: eslint使用npm的实现

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['./eslint/vue3-ts-typecheck.cjs', 'prettier'],
+  extends: ['plugin:@iskl/vue-ts-typecheck', 'prettier'],
   overrides: [
     {
       // 根目录层级的*.ts没有纳入tsconfig的project，所以在这里设置为project为null，不进行type checking
--- eslint/vue3.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    'vue/setup-compiler-macros': true,
-  },
-  extends: [require.resolve('./base.cjs'), 'plugin:vue/vue3-recommended'],
-};
